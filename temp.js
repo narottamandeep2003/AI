87ae3feed531049190e7146ee36b0c87cc1327b5
Mandeep singh <narotta2003@gmail.com>
2023-02-24 12:21:12 +0530

    initial commit

--- temp.js
+++ temp.js
@@ -1,150 +1,185 @@
-let  queue=[]
-class node{
-    constructor(val){
-       this.data=val
-       
+let queue = []
+class node {
+    constructor(val) {
+        this.data = val
+
     }
 
 }
-
-class space{
-    constructor(data,val){
-        this.data=new node(data)
-        this.root=val;
-        this.child=[]
+class space {
+    constructor(data, val) {
+        this.data = new node(data)
+        this.root = val;
+        this.child = []
     }
-    addchild=(data)=>{
-     this.child.push(new space(data,this))
-    }
+    // addchild=(data)=>{
+    //  this.child.push(new space(data,this))
+    // }
 }
 
-let s =new space([0,0],null);
-queue.push([0,0])
-// queue.forEach((a)=>{
-//     console.log(a.a,a.b)
-// })
+let s = new space([0, 0], null);
+queue.push([0, 0])
+let size = [4, 9]
+let goal = [0, 6]
 
-let size=[4,3]
-let goal=[0,1]
-
-let generate=(obj)=>{
-    let currSize=obj.data.data
- 
+let generate = (obj) => {
+    let currSize = obj.data.data
+    if (currSize[0] == goal[0] && currSize[1] == goal[1]) {
+        return
+    }
 
 
     // full a
-    let childval=[size[0],currSize[1]]
-   
-    let flag=true;
-    queue.forEach((o)=>{
-        if(o[0]==childval[0] && o[1]==childval[1]){
-            flag=false
+    let childval = [size[0], currSize[1]]
+
+    let flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
         }
     })
-    if(flag){
+    if (flag) {
         queue.push(childval)
-        obj.child.push(new space(childval,obj))
-        
+        obj.child.push(new space(childval, obj))
+
+    }
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
     }
 
-   // full b
-    childval=[currSize[0],size[1]]
-    flag=true;
-    queue.forEach((o)=>{
-        if(o[0]==childval[0] && o[1]==childval[1]){
-            flag=false
+    // full b
+    childval = [currSize[0], size[1]]
+    flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
         }
     })
-    if(flag){
+    if (flag) {
         queue.push(childval)
-        obj.child.push(new space(childval,obj))
+        obj.child.push(new space(childval, obj))
+    }
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
     }
 
     // transfer a to b
 
 
-    let num=size[1]-currSize[1]
-    if(currSize[0]<=num){
-        childval=[0,(currSize[0]+currSize[1])]
+    let num = size[1] - currSize[1]
+    if (currSize[0] <= num) {
+        childval = [0, (currSize[0] + currSize[1])]
     }
-    else{
-        childval=[currSize[0]-num,currSize[1]+num]
+    else {
+        childval = [currSize[0] - num, currSize[1] + num]
     }
 
-    flag=true;
-    queue.forEach((o)=>{
-        if(o[0]==childval[0] && o[1]==childval[1]){
-            flag=false
+
+    flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
         }
     })
-    if(flag){
+    if (flag) {
         queue.push(childval)
-        obj.child.push(new space(childval,obj))
+        obj.child.push(new space(childval, obj))
     }
-     // transfer b to a
- 
-     num=size[0]-currSize[0]
-     if(currSize[1]<=num){
-         childval=[currSize[0]+currSize[1],0] 
-     }
-     else{
-        childval=[currSize[0]+num,currSize[1]-num] 
-       
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
     }
 
-    flag=true;
-     queue.forEach((o)=>{
-         if(o[0]==childval[0] && o[1]==childval[1]){
-             flag=false
-         }
-     })
-     if(flag){
-         queue.push(childval)
-         obj.child.push(new space(childval,obj))
-     }
+    // transfer b to a
+
+    num = size[0] - currSize[0]
+    if (currSize[1] <= num) {
+        childval = [currSize[0] + currSize[1], 0]
+    }
+    else {
+        childval = [currSize[0] + num, currSize[1] - num]
+
+    }
+    flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
+        }
+    })
+    if (flag) {
+        queue.push(childval)
+        obj.child.push(new space(childval, obj))
+    }
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
+    }
 
 
 
 
     //  empty a
-    childval=[0,currSize[1]]
+    childval = [0, currSize[1]]
 
-    flag=true;
-    queue.forEach((o)=>{
-        if(o[0]==childval[0] && o[1]==childval[1]){
-            flag=false
+    flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
         }
     })
-    if(flag){
+    if (flag) {
         queue.push(childval)
-        obj.child.push(new space(childval,obj))
+        obj.child.push(new space(childval, obj))
+    }
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
     }
 
     // empty b
-    childval=[currSize[0],0]
-    flag=true;
-    queue.forEach((o)=>{
-        if(o[0]==childval[0] && o[1]==childval[1]){
-            flag=false
+    childval = [currSize[0], 0]
+    flag = true;
+    queue.forEach((o) => {
+        if (o[0] == childval[0] && o[1] == childval[1]) {
+            flag = false
         }
     })
-    if(flag){
+    if (flag) {
         queue.push(childval)
-        obj.child.push(new space(childval,obj))
+        obj.child.push(new space(childval, obj))
     }
-    obj.child.forEach((i)=>{
-       generate(i)
+    else if (childval[0] == goal[0] && childval[1] == goal[1]) {
+        obj.child.push(new space(childval, obj))
+    }
+    obj.child.forEach((i) => {
+        generate(i)
     })
 }
 
 generate(s)
 
+let visitnode=[]
+visitnode.push(s);
 
+let bfs=()=>{
 
-let visit=(s)=>{
-    console.log(s.data)
-    s.child.forEach((i)=>{
-        visit(i)
-    })
+    while(visitnode.length!=0){
+       let node= visitnode[0].data
+       if(node.data[0]==goal[0] && node.data[1]==goal[1])
+       {
+        console.log("dmdmd")
+        return visitnode[0];
+       }
+       visitnode[0].child.forEach((childnode)=>{
+        visitnode.push(childnode)
+       })
+       visitnode.shift()
+    }
+
 }
-visit(s)
+let result=bfs();
+let ans=[]
+
+while(result.root!=null){
+    ans.push(result.data.data)
+    result=result.root
+}
+ans.push(result.data.data)
+console.table(ans.reverse())
--- tempCodeRunnerFile.js
+++ tempCodeRunnerFile.js
@@ -1,3 +1,1 @@
-else{
-    //      childval=[size[0],(currSize[1]-num)]
-    //  }
+visitnode
